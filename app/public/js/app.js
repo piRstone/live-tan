63a833f7e1af777b826f88d5eb8a04e53f6a6951
piRstone <pierre.lavalley@gmail.com>
2017-05-31 10:13:07 +0200

    gestion des erreurs

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,25 +1,38 @@
-// (function() {
-// })()
+var nbErrors = 0;
 
+function updateDOM(data, index, elementId) {
+    var rawTime = data[index].temps.split(' ');
+    var time = rawTime[0];
+    var next = document.getElementById(elementId);
+    next.innerText = time;
+}
 
 function call(success) {
     $.ajax({
         url: '/horaires',
         method: 'GET',
         success: function(data) {
-            var rawTime = data[0].temps.split(' ');
-            var time = rawTime[0];
-            var next = document.getElementById('next');
-            next.innerText = time;
+            updateDOM(data, 0, 'next');
+            updateDOM(data, 1, 'next2');
+            data[2] != undefined && updateDOM(data, 2, 'next3');
+            success();
+        },
+        error: function(err) {
+            // Arrête l'interval au bout de 10 erreurs
+            nbErrors++;
+            if (nbErrors >= 10) {
+                clearInterval(interval);
+                document.getElementById('error').style.display = "block";
+            }
             success();
         }
     });
 }
-call();
+call(function() {});
 
-setInterval(function() {
+var interval = setInterval(function() {
     document.getElementById('refresh').style.visibility = "visible";
     call(function() {
         document.getElementById('refresh').style.visibility = "hidden";
     });
-}, 10000)
+}, 10000);
